731ff0833005f476ffee372de0f5c60ea880483d
Lepha <lepharamchiary@gmail.com>
2024-08-29 10:59:23 +0530

    complete get all cards controllers

--- api/src/controllers/card.controller.js
+++ api/src/controllers/card.controller.js
@@ -3,18 +3,23 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
 const createCard = async (req, res) => {
- const { title, description} = req.body
- try {
-    const newCard = new Card({title, description})
-    await newCard.save()
-    res.status(201).json(new ApiResponse(201, newCard, "Card crearted successfully"))
- } catch (error) {
-    throw new ApiError(500,"Something went wrong while creating the card")
- }
+    const { title, description } = req.body
+    try {
+        const newCard = new Card({ title, description })
+        await newCard.save()
+        res.status(201).json(new ApiResponse(201, newCard, "Card crearted successfully"))
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while creating the card")
+    }
 }
 
 const getAllCard = async (req, res) => {
-
+    try {
+        const cards = await Card.find();
+        res.status(200).json(new ApiResponse(200, cards, "Cards fetched successfully"))
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while fetching cards")
+    }
 }
 
-export { getAllCard , createCard}
+export { getAllCard, createCard }
