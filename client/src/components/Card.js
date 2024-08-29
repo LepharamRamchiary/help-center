8d2ddc9ca2bd1367c3d431b31fd00deedd0235b9
Lepha <lepharamchiary@gmail.com>
2024-08-29 16:08:53 +0530

    simple changes

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Card = ({ title, description }) => {
   return (
-    <div className="border border-gray-300 rounded shadow h-32 bg-slate-200 divide-y divide-gray-300">
+    <div className="border border-gray-300 rounded shadow bg-slate-200 divide-y divide-gray-300">
       <h2 className="text-xl font-bold mb-2 px-2">{title}</h2>
-      <p className='px-2'>{description}</p>
+      <p className='px-2 py-1'>{description}</p>
     </div>
   );
 };
